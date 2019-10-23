41b4bccd007d635ec6e5273c8df051794ceb0a70
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-23 10:01:06 -0500

    Se ajustan rutas por nueva ubicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)},
+  { path: 'intro', loadChildren: './pages/intro/intro.module#IntroPageModule' },
 ];
 
 @NgModule({
