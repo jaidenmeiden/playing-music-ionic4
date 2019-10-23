37b5b79cc3fdbd3cba536a63b38cccb9b03ea5b9
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-23 09:32:24 -0500

    Slides dinámicos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  slidesOpts = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 400
+  };
+
+  slides = [{
+    imagenSrc: "./assets/img/logo.png",
+    imagenAlt: "Platzy Music Logo",
+    title: "Este es el título 1",
+    subTitle: "Este es el sub título 1",
+    description: "Aquí va la descripción 1",
+    icon: "play"
+  }, {
+    imagenSrc: "./assets/img/logo.png",
+    imagenAlt: "Platzy Music Logo",
+    title: "Este es el título 2",
+    subTitle: "Este es el sub título 2",
+    description: "Aquí va la descripción 2",
+    icon: "play"
+  }, {
+    imagenSrc: "./assets/img/logo.png",
+    imagenAlt: "Platzy Music Logo",
+    title: "Este es el título 3",
+    subTitle: "Este es el sub título 3",
+    description: "Aquí va la descripción 3",
+    icon: "play"
+  }];
 
   constructor() {}
 
