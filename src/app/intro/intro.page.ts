fa88b87a14cf9c55546a2fb323d5dba73c2184c7
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-23 09:56:39 -0500

    Se almacena variable y se aplica el router

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {Storage} from "@ionic/storage";
 
 @Component({
   selector: 'app-intro',
@@ -37,9 +39,17 @@ export class IntroPage implements OnInit {
     icon: "play"
   }];
 
-  constructor() { }
+  constructor(
+      private router: Router,
+      private storage: Storage
+  ) { }
 
   ngOnInit() {
+  }
+
+  finish() {
+    this.storage.set('isIntroShowed', true);
+    this.router.navigateByUrl("/home");
   }
 
 }
