bfc2e8bc00642803f4b478640bedc0f975d4de5c
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-24 12:26:59 -0500

    Uso de componente Slides y opciones avanzadas

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  artists = [{}, {}, {}, {}, {}, {}, {}, {}];
+  slideOps = {
+    initialSlide: 2,
+    slidesPerView: 4,
+    centeredSlides: true,
+    speed: 400
+  };
   constructor() {}
 
 }
