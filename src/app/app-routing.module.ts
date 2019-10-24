5e93e6ef4a34ea7ce94574daeb8babb981f3f2d7
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-24 11:56:04 -0500

    Agregar un menú

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   { path: 'intro', loadChildren: './pages/intro/intro.module#IntroPageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
 ];
 
 @NgModule({
