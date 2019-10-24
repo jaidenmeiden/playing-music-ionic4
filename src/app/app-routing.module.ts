ea39aecbc8dcefffb39e3384ec977009c0c44b34
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-24 10:51:34 -0500

    Agregar Guards a nuestro login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {IntroGuard} from "./guards/intro.guard";
+import {LoginGuard} from "./guards/login.guard";
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
-    canActivate: [IntroGuard]
+    canActivate: [LoginGuard, IntroGuard]
   },
   { path: 'intro', loadChildren: './pages/intro/intro.module#IntroPageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,6 +7,7 @@ import {
 } from "@angular/forms";
 import {NavController} from "@ionic/angular";
 import {AuthenticateService} from "../../services/authenticate.service";
+import {Storage} from "@ionic/storage";
 
 @Component({
   selector: 'app-login',
@@ -31,7 +32,8 @@ export class LoginPage implements OnInit {
   constructor(
       private formBuilder: FormBuilder,
       private authService: AuthenticateService,
-      private navCtrl: NavController
+      private navCtrl: NavController,
+      private storage: Storage
   ) {
     this.loginForm = this.formBuilder.group({
       email: new FormControl(
@@ -51,11 +53,17 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
-  loginUser(credentials) {
-    this.authService.loginUser(credentials).then(res => {
-      this.errorMessage = "";
-      this.navCtrl.navigateForward("/home");//Navega hacia adelante para mostrar una transición
-    });
-  }
+    loginUser(credentials) {
+        this.authService
+            .loginUser(credentials)
+            .then(res => {
+                this.errorMessage = "";
+                this.storage.set("isUserLoggedIn", true);
+                this.navCtrl.navigateForward("/home");
+            })
+            .catch(err => {
+                this.errorMessage = err;
+            });
+    }
 
 }
