f3bdfbc03ded94a780384d64cdd5e9b57df736d4
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-24 12:04:31 -0500

    Agregar componente Menu al menú, proteger la pagina del menu con los guards, eliminar la ruta directa al home e incluir home como hijo de menu, llamando el nuemo dese el home.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,12 @@ import {LoginGuard} from "./guards/login.guard";
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
-    canActivate: [LoginGuard, IntroGuard]
-  },
   { path: 'intro', loadChildren: './pages/intro/intro.module#IntroPageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
-  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule',
+    canActivate: [LoginGuard, IntroGuard]
+  }
 ];
 
 @NgModule({
--- src/app/pages/menu/menu.module.ts
+++ src/app/pages/menu/menu.module.ts
@@ -10,7 +10,14 @@ import { MenuPage } from './menu.page';
 const routes: Routes = [
   {
     path: '',
-    component: MenuPage
+    component: MenuPage,
+    children: [
+      {
+        path: "home",
+        loadChildren: () =>
+            import("../home/home.module").then(m => m.HomePageModule)
+      }
+    ]
   }
 ];
 
