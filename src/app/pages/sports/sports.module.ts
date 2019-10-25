e2fc8e21388e3f2c97899dbb41caf45b76abb81d
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-24 20:27:46 -0500

    Página Sports y Angular Maps incluyento una API key

--- src/app/pages/sports/sports.module.ts
+++ src/app/pages/sports/sports.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { SportsPage } from './sports.page';
+import { AgmCoreModule } from "@agm/core";
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    AgmCoreModule,
     RouterModule.forChild(routes)
   ],
   declarations: [SportsPage]
